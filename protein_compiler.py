e9cfe8b02096483352663600a6e4c0c54ce863a1
JustinWayland <justintwayland+github@gmail.com>
2021-09-28 17:08:17 -0400

    Create protein_compiler.py
    Started python file with hard-coded RNA codons.

---
+++ protein_compiler.py
@@ -0,0 +1,26 @@
+protein_codon = {
+	"A": ["GCU", "GCC", "GCA", "GCG"],
+	"R": ["CGU", "CGC", "CGA", "CGG", "AGA", "AGG"],
+	"N": ["AAU","AAC"],
+	"D": ["GAU", "GAC"],
+	"B": ["AAU", "AAC", "GAU", "GAC"],
+	"C": ["UGU", "UGC"],
+	"Q": ["CAA", "CAG"],
+	"E": ["GAA", "GAG"],
+	"Z": ["CAA", "CAG", "GAA", "GAG"],
+	"G": ["GGU", "GGC", "GGA", "GGG"],
+	"H": ["CAU", "CAC"],
+	"I": ["AUU", "AUC", "AUA"],
+	"L": ["CUU", "CUC", "CUA", "CUG", "UUA", "UUG"],
+	"K": ["AAA", "AAG"],
+	"M": ["AUG"],
+	"F": ["UUU", "UUC"],
+	"P": ["CCU", "CCC", "CCA", "CCG"],
+	"S": ["UCU", "UCC", "UCA", "UCG", "AGU", "AGC"],
+	"T": ["ACU", "ACC", "ACA", "ACG"],
+	"W": ["UGG"],
+	"Y": ["UAU", "UAC"],
+	"V": ["GUU", "GUC", "GUA", "GUG"]
+}
+start_codon = "AUG"
+stop_codons = ["UAA", "UGA", "UAG"]
